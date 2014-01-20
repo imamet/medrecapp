1083b4b44760cfc57718a67d6ec6df9a9aa83ed5
herna <hernawatisamosir@gmail.com>
2014-01-20 11:01:17 -0800

    Commit-5 By Herna (Obat)

--- src/medrecapp/Gui/FrmUtama.java
+++ src/medrecapp/Gui/FrmUtama.java
@@ -14,6 +14,7 @@ package medrecapp.Gui;
 import medrecapp.Gui.Internal.FrmIntSpesialis;
 import medrecapp.Gui.Internal.FrmIntJaminan;
 import java.awt.Dimension;
+import medrecapp.Gui.Internal.FrmIntListPasien;
 import medrecapp.Gui.Internal.FrmIntObat;
 import medrecapp.Gui.Internal.FrmIntStaf;
 
@@ -73,7 +74,6 @@ public class FrmUtama extends javax.swing.JFrame {
 
         menuFile.setText("File");
 
-        menuExit.setText("Exit");
         menuExit.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 menuExitActionPerformed(evt);
@@ -276,7 +276,16 @@ public class FrmUtama extends javax.swing.JFrame {
 
     private void menuPasienActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_menuPasienActionPerformed
         // TODO add your handling code here:
-        
+        jDesktopPane1.removeAll();
+        jDesktopPane1.repaint();
+        FrmIntListPasien fip = new FrmIntListPasien();
+        fip.setSize(jDesktopPane1.getSize());        /**************** MAXIMIZE Internal Frame *****************/
+        Dimension parentSize = jDesktopPane1.getSize();
+        Dimension childSize = fip.getSize();
+        fip.setLocation((parentSize.width - childSize.width)/2, (parentSize.height - childSize.height)/2);
+        jDesktopPane1.add(fip);
+        fip.show();
+        fip.toFront();
     }//GEN-LAST:event_menuPasienActionPerformed
 
     private void menuJaminanActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_menuJaminanActionPerformed
